dd53c5fcedb2e305a5be51759bdbcf16bf1aadab
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-06-02 09:52:59 -0500

    Operaciones en JS
    Suma básica en JavaScript.

---
+++ js/suma.js
@@ -0,0 +1,7 @@
+function sumaNumeros() {
+  let num1 = 5;
+  let num2 = 10;
+  document.write('Mi práctica...Karina Bautista Bautista'+ "<br>");
+
+  document.write("<br>"+ "La suma de 5 + 10 es igual a... ", num1 + num2);
+}
