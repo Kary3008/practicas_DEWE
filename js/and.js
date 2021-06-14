199776bb3c8e29afad79c555a5aff2e562e4c40f
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2021-06-14 13:58:01 -0500

    Prácticas con JS
    Operadores lógicos: AND con JavaScript.
    14/06/2021

---
+++ js/and.js
@@ -0,0 +1,7 @@
+function operadorAnd() {
+  result = a && b;
+  alert(true && true); // true
+  alert(false && true); // false
+  alert(true && false); // false
+  alert(false && false); // false
+}
